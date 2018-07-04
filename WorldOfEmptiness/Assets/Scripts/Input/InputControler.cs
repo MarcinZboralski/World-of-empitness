61004be1d7a29c4497c01a43161de44ef3e3a3a8
norbertt.lubaszka <norbertt.lubaszka@outlook.com>
2018-07-04 14:07:41 +0200

    Input controler, advanced version.

--- WorldOfEmptiness/Assets/Scripts/Input/InputControler.cs
+++ WorldOfEmptiness/Assets/Scripts/Input/InputControler.cs
@@ -4,104 +4,326 @@ using UnityEngine;
 
 public class InputControler : MonoBehaviour
 {
-	void Update () 
+	List<KeyCode> myKeys = new List<KeyCode>();
+	string debuglogInfo = "User pressed: ";
+	void Start()
 	{
 		#region WSAD
-		if (Input.GetKey (KeyCode.A))
-			Debug.Log ("User used: A");
-		if (Input.GetKey (KeyCode.W))
-			Debug.Log ("User used: W");
-		if (Input.GetKey (KeyCode.S))
-			Debug.Log ("User used: S");
-		if (Input.GetKey (KeyCode.D))
-			Debug.Log ("User used: D");
+		myKeys.Add (KeyCode.A);
+		myKeys.Add (KeyCode.W);
+		myKeys.Add (KeyCode.S);
+		myKeys.Add (KeyCode.D);
 		#endregion
 
 		#region Arrows
-		if (Input.GetKey (KeyCode.DownArrow))
-			Debug.Log ("User used: DownArrow");
-		if (Input.GetKey (KeyCode.UpArrow))
-			Debug.Log ("User used: UpArrow");
-		if (Input.GetKey (KeyCode.RightArrow))
-			Debug.Log ("User used: RightArrow");
-		if (Input.GetKey (KeyCode.LeftArrow))
-			Debug.Log ("User used: Left Arrow");
+		myKeys.Add (KeyCode.UpArrow);
+		myKeys.Add (KeyCode.DownArrow);
+		myKeys.Add (KeyCode.LeftArrow);
+		myKeys.Add (KeyCode.RightArrow);
 		#endregion
 
-		#region AlphaNumeric
-		if (Input.GetKey (KeyCode.Alpha1))
-			Debug.Log ("User used: 1");
-		if (Input.GetKey (KeyCode.Alpha2))
-			Debug.Log ("User used: 2");
-		if (Input.GetKey (KeyCode.Alpha3))
-			Debug.Log ("User used: 3");
-		if (Input.GetKey (KeyCode.Alpha4))
-			Debug.Log ("User used: 4");
-		if (Input.GetKey (KeyCode.Alpha5))
-			Debug.Log ("User used: 5");
-		if (Input.GetKey (KeyCode.Alpha6))
-			Debug.Log ("User used: 6");
-		if (Input.GetKey (KeyCode.Alpha7))
-			Debug.Log ("User used: 7");
-		if (Input.GetKey (KeyCode.Alpha8))
-			Debug.Log ("User used: 8");
-		if (Input.GetKey (KeyCode.Alpha9))
-			Debug.Log ("User used: 9");
-		if (Input.GetKey (KeyCode.Alpha0))
-			Debug.Log ("User used: 0");
+		#region 1-0 Up
+		myKeys.Add (KeyCode.Alpha0);
+		myKeys.Add (KeyCode.Alpha1);
+		myKeys.Add (KeyCode.Alpha2);
+		myKeys.Add (KeyCode.Alpha3);
+		myKeys.Add (KeyCode.Alpha4);
+		myKeys.Add (KeyCode.Alpha5);
+		myKeys.Add (KeyCode.Alpha6);
+		myKeys.Add (KeyCode.Alpha7);
+		myKeys.Add (KeyCode.Alpha8);
+		myKeys.Add (KeyCode.Alpha9);
+		#endregion
+
+		#region 1-0 Side
+		myKeys.Add (KeyCode.Keypad0);
+		myKeys.Add (KeyCode.Keypad1);
+		myKeys.Add (KeyCode.Keypad2);
+		myKeys.Add (KeyCode.Keypad3);
+		myKeys.Add (KeyCode.Keypad4);
+		myKeys.Add (KeyCode.Keypad5);
+		myKeys.Add (KeyCode.Keypad6);
+		myKeys.Add (KeyCode.Keypad7);
+		myKeys.Add (KeyCode.Keypad8);
+		myKeys.Add (KeyCode.Keypad9);
+		#endregion
+
+		#region F1-F12
+		myKeys.Add (KeyCode.F1);
+		myKeys.Add (KeyCode.F2);
+		myKeys.Add (KeyCode.F3);
+		myKeys.Add (KeyCode.F4);
+		myKeys.Add (KeyCode.F5);
+		myKeys.Add (KeyCode.F6);
+		myKeys.Add (KeyCode.F7);
+		myKeys.Add (KeyCode.F8);
+		myKeys.Add (KeyCode.F9);
+		myKeys.Add (KeyCode.F10);
+		myKeys.Add (KeyCode.F11);
+		myKeys.Add (KeyCode.F12);
 		#endregion
 
 		#region Q-P
-		if (Input.GetKey (KeyCode.Q))
-			Debug.Log ("User used: Q");
-		if (Input.GetKey (KeyCode.E))
-			Debug.Log ("User used: E");
-		if (Input.GetKey (KeyCode.R))
-			Debug.Log ("User used: R");
-		if (Input.GetKey (KeyCode.T))
-			Debug.Log ("User used: T");
-		if (Input.GetKey (KeyCode.Y))
-			Debug.Log ("User used: Y");
-		if (Input.GetKey (KeyCode.U))
-			Debug.Log ("User used: U");
-		if (Input.GetKey (KeyCode.I))
-			Debug.Log ("User used: I");
-		if (Input.GetKey (KeyCode.O))
-			Debug.Log ("User used: O");
-		if (Input.GetKey (KeyCode.P))
-			Debug.Log ("User used: P");
+		myKeys.Add (KeyCode.Q);
+		myKeys.Add (KeyCode.E);
+		myKeys.Add (KeyCode.R);
+		myKeys.Add (KeyCode.T);
+		myKeys.Add (KeyCode.Y);
+		myKeys.Add (KeyCode.U);
+		myKeys.Add (KeyCode.I);
+		myKeys.Add (KeyCode.O);
+		myKeys.Add (KeyCode.P);
 		#endregion
 
 		#region F-L
-		if (Input.GetKey (KeyCode.F))
-			Debug.Log ("User used: F");
-		if (Input.GetKey (KeyCode.G))
-			Debug.Log ("User used: G");
-		if (Input.GetKey (KeyCode.H)
-			Debug.Log ("User used: H");
-		if (Input.GetKey (KeyCode.J))
-			Debug.Log ("User used: J");
-		if (Input.GetKey (KeyCode.K))
-			Debug.Log ("User used: K");
-		if (Input.GetKey (KeyCode.L))
-			Debug.Log ("User used: L");
+		myKeys.Add (KeyCode.F);
+		myKeys.Add (KeyCode.G);
+		myKeys.Add (KeyCode.H);
+		myKeys.Add (KeyCode.J);
+		myKeys.Add (KeyCode.K);
+		myKeys.Add (KeyCode.L);
 		#endregion
 
 		#region Z-M
-		if (Input.GetKey (KeyCode.Z))
-			Debug.Log ("User used: Z");
-		if (Input.GetKey (KeyCode.X))
-			Debug.Log ("User used: X");
-		if (Input.GetKey (KeyCode.C))
-			Debug.Log ("User used: C");
-		if (Input.GetKey (KeyCode.V))
-			Debug.Log ("User used: V");
-		if (Input.GetKey (KeyCode.B))
-			Debug.Log ("User used: B");
-		if (Input.GetKey (KeyCode.N))
-			Debug.Log ("User used: N");
-		if (Input.GetKey (KeyCode.M))
-			Debug.Log ("User used: M");
+		myKeys.Add (KeyCode.Z);
+		myKeys.Add (KeyCode.X);
+		myKeys.Add (KeyCode.C);
+		myKeys.Add (KeyCode.V);
+		myKeys.Add (KeyCode.B);
+		myKeys.Add (KeyCode.N);
+		myKeys.Add (KeyCode.M);
 		#endregion
+
+		#region OTHERS
+		myKeys.Add(KeyCode.Escape);
+		#endregion
+	}
+
+	void Update () 
+	{
+		foreach (KeyCode keyCode in myKeys) 
+		{
+			if (Input.GetKey (keyCode)) 
+			{
+				switch (keyCode)
+				{
+				#region WSAD
+				case KeyCode.A:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.W:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.S:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.D:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				#endregion
+
+				#region Arrows
+				case KeyCode.UpArrow:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.DownArrow:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.LeftArrow:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.RightArrow:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				#endregion
+
+				#region 1-0 Up
+				case KeyCode.Alpha0:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.Alpha1:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.Alpha2:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.Alpha3:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.Alpha4:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.Alpha5:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.Alpha6:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.Alpha7:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.Alpha8:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.Alpha9:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				#endregion
+				
+				#region 1-0 Side
+				case KeyCode.Keypad0:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.Keypad1:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.Keypad2:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.Keypad3:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.Keypad4:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.Keypad5:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.Keypad6:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.Keypad7:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.Keypad8:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.Keypad9:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				#endregion
+
+				#region F1-F12
+				case KeyCode.F1:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.F2:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.F3:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.F4:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.F5:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.F6:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.F7:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.F8:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.F9:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.F10:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.F11:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.F12:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				#endregion
+
+				#region Q-P
+				case KeyCode.Q:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.E:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.R:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.T:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.Y:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.U:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.I:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.O:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.P:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				#endregion
+
+				#region F-L
+				case KeyCode.F:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.G:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.H:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.J:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.K:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.L:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				#endregion
+
+				#region Z-M
+				case KeyCode.Z:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.X:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.C:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.V:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.B:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.N:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				case KeyCode.M:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				#endregion
+
+				#region OTHERS
+				case KeyCode.Escape:
+					Debug.Log(debuglogInfo + keyCode.ToString());
+					break;
+				#endregion
+				}
+			}
+		}
 	}
 }
