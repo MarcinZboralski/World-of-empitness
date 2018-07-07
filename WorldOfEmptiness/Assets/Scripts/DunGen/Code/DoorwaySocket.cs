1875c0e5661649b0823412882f1f5a48af83e065
Marcin Zboralski <32996203+MarcinZboralski@users.noreply.github.com>
2018-07-07 19:15:44 +0200

    Added Basic Bounds

--- WorldOfEmptiness/Assets/Scripts/DunGen/Code/DoorwaySocket.cs
+++ WorldOfEmptiness/Assets/Scripts/DunGen/Code/DoorwaySocket.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace DunGen
 {
@@ -22,7 +23,7 @@ namespace DunGen
 	{
         Default,
         Large,
-        Vertical,
+        Vertical
 	}
 
     /// <summary>
---
+++ WorldOfEmptiness/Assets/Scripts/Utility/AutoBounds.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using DunGen;
+using UnityEngine;
+
+public sealed class AutoBounds : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawCube(transform.position,UnityUtil.CalculateObjectBounds(gameObject,false,false,false).size);
+    }
+}
